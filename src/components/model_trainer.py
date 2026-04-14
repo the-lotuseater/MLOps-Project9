001acf86704328fa2912db1c590618f4d222de89
Abhishek Birhade <abirhade@gatechu.edu>
2026-04-14 17:19:22 -0500

    Add GridSearchCV for hyperparam tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,6 +13,7 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_obj
+from sklearn.model_selection import GridSearchCV
 
 @dataclass
 class ModelTrainerConfig:
@@ -32,12 +33,48 @@ class ModelTrainer:
                 'Decision Tree': DecisionTreeRegressor(),
                 'Gradient Boosting': GradientBoostingRegressor(),
                 'Linear Regression': LinearRegression(),
-                'K-Neighbors Classifier': KNeighborsRegressor(),
-                'XGBClassifier': XGBRegressor(),
-                'CatBoosting Classifier': CatBoostRegressor(),
-                'AdaBoost Classifier': AdaBoostRegressor()
+                'XGBRegressor': XGBRegressor(),
+                'CatBoosting Regressor': CatBoostRegressor(),
+                'AdaBoost Regressor': AdaBoostRegressor()
             }
-            model_report: dict = self.evaluate_model(X_train,X_test,y_train,y_test,models)
+            hyper_params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report: dict = self.evaluate_model(X_train,X_test,y_train,y_test,models,hyper_params)
             best_model_name = max(model_report,key=model_report.get)
             best_model_score = model_report[best_model_name]
             if best_model_score<0.6:
@@ -57,11 +94,17 @@ class ModelTrainer:
         except Exception as e:
             raise CustomException(e,sys)
     
-    def evaluate_model(self,X_train, X_test, y_train, y_test,models):
+    def evaluate_model(self,X_train, X_test, y_train, y_test,models,hyper_params):
         try:
             report = {}
             for model_name,model in models.items():
+                params = hyper_params[model_name]
+                gs = GridSearchCV(model,params,cv=3)
+                gs.fit(X_train,y_train)
+
+                model.set_params(**gs.best_params_)
                 model.fit(X_train,y_train)
+                
                 y_test_pred = model.predict(X_test)
                 test_model_score = r2_score(y_test,y_test_pred)
                 report[model_name] = test_model_score
