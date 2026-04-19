a007cb194590d8d6780d17db573553a5daa78ea5
Abhishek Birhade <abirhade@gatechu.edu>
2026-04-19 14:21:31 -0500

    Update application.py

--- application.py
+++ application.py
@@ -7,14 +7,13 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application = Flask(__name__)
 
-app =application
 predict_pipeline = PredictPipeline()
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods = ['GET','POST'])
+@application.route('/predictdata',methods = ['GET','POST'])
 def predict():
     if request.method=='GET':
         return render_template('home.html')
@@ -33,4 +32,4 @@ def predict():
         return render_template('home.html', results = results[0])
     
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=8000)
+    application.run(host='0.0.0.0', port=8000)
